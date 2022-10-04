de357a3f6c6e8f647a40c3acb1605cef7ffb7737
Charawey-X <charawey@gmail.com>
2022-10-04 08:13:26 +0300

    add benefits and pricing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
 import Programs from './components/Programs/Programs';
+import Pricing from './components/Pricing/Pricing';
+import Benefits from './components/Benefits/Benefits';
 
 function App() {
   return (
     <div className="App">
       <Hero></Hero>
       <Programs></Programs>
+      <Benefits></Benefits>
+      <Pricing></Pricing>
     </div>
   );
 }
